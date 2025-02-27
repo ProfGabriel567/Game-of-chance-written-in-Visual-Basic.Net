ef26c05b6ff68cf7e4f79ffc76ea9aaa77e9ef23
Gabriel Avendaño Alvarez <gabriel_avendao@hotmail.com>
2025-02-26 23:15:05 -0600

    Add files via upload

---
+++ Juego de la Fortuna/Juego de la Fortuna/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFortuna
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFortuna))
+        Me.btnJugar = New System.Windows.Forms.Button()
+        Me.btnGameOver = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnJugar
+        '
+        Me.btnJugar.Location = New System.Drawing.Point(56, 61)
+        Me.btnJugar.Name = "btnJugar"
+        Me.btnJugar.Size = New System.Drawing.Size(123, 23)
+        Me.btnJugar.TabIndex = 0
+        Me.btnJugar.Text = "¡ A JUGAR !"
+        Me.btnJugar.UseVisualStyleBackColor = True
+        '
+        'btnGameOver
+        '
+        Me.btnGameOver.Location = New System.Drawing.Point(56, 104)
+        Me.btnGameOver.Name = "btnGameOver"
+        Me.btnGameOver.Size = New System.Drawing.Size(123, 23)
+        Me.btnGameOver.TabIndex = 1
+        Me.btnGameOver.Text = "GAME OVER"
+        Me.btnGameOver.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(222, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 36)
+        Me.Label1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(361, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 36)
+        Me.Label2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(489, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 36)
+        Me.Label3.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(206, 20)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(376, 29)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "¡ ENCUENTRA EL 7 GANADOR !"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Juego_de_la_Fortuna.My.Resources.Resources.monedas_de_oro
+        Me.PictureBox1.Location = New System.Drawing.Point(47, 316)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(484, 270)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label5.Location = New System.Drawing.Point(47, 254)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(327, 34)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "¡ ERES UN GANADOR !"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(53, 210)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "SCORE:"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Location = New System.Drawing.Point(130, 210)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(39, 13)
+        Me.lblScore.TabIndex = 9
+        Me.lblScore.Text = "Label7"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(56, 316)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(484, 270)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(47, 316)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(493, 270)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 12
+        Me.PictureBox4.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Ravie", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(231, 190)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(323, 39)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "¡ GAME OVER  !"
+        '
+        'frmFortuna
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(603, 620)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGameOver)
+        Me.Controls.Add(Me.btnJugar)
+        Me.Name = "frmFortuna"
+        Me.Text = "EL JUEGO DE LA FORTUNA DIGITAL"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnJugar As System.Windows.Forms.Button
+    Friend WithEvents btnGameOver As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Juego de la Fortuna/Juego de la Fortuna/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class frmFortuna
+
+    Public score As Integer = 100
+    Private Sub btnGameOver_Click(sender As Object, e As EventArgs) Handles btnGameOver.Click
+        End
+    End Sub
+
+    Private Sub btnJugar_Click(sender As Object, e As EventArgs) Handles btnJugar.Click
+
+        Label5.Visible = False
+        PictureBox1.Visible = False
+        PictureBox3.Visible = False
+        PictureBox4.Visible = False
+        Label1.Text = CStr(Int(Rnd() * 10))
+        Label2.Text = CStr(Int(Rnd() * 10))
+        Label3.Text = CStr(Int(Rnd() * 10))
+
+        If (Label1.Text = "7") Or (Label2.Text = "7") Or (Label3.Text = "7") Then
+            Label5.Visible = True
+            PictureBox1.Visible = True
+            Beep()
+
+        End If
+
+
+        If lblScore.Text <= "0" Then
+            btnJugar.Visible = False
+
+            lblScore.Visible = False
+            Label5.Text = "GAME OVER"
+            PictureBox1.Visible = False
+            PictureBox4.Visible = False
+            PictureBox3.Visible = True
+            Beep()
+            'System.Threading.Thread.Sleep(3000)
+            'End
+        End If
+
+
+
+        If Label1.Text = "7" And Label2.Text = "7" And Label3.Text = "7" Then
+            lblScore.Text = lblScore.Text + 100
+        ElseIf Label1.Text = "7" And Label2.Text = "7" And Label3.Text <> "7" Then
+            lblScore.Text = lblScore.Text + 40
+        ElseIf Label1.Text = "7" And Label2.Text <> "7" And Label3.Text = "7" Then
+            lblScore.Text = lblScore.Text + 40
+        ElseIf Label1.Text <> "7" And Label2.Text = "7" And Label3.Text = "7" Then
+            lblScore.Text = lblScore.Text + 40
+        ElseIf Label1.Text <> "7" And Label2.Text <> "7" And Label3.Text = "7" Then
+            lblScore.Text = lblScore.Text + 20
+        ElseIf Label1.Text <> "7" And Label2.Text = "7" And Label3.Text <> "7" Then
+            lblScore.Text = lblScore.Text + 20
+        ElseIf Label1.Text = "7" And Label2.Text <> "7" And Label3.Text <> "7" Then
+        Else
+            If lblScore.Text > 0 Then
+
+
+                lblScore.Text = lblScore.Text - 10
+                PictureBox1.Visible = False
+                PictureBox3.Visible = False
+                PictureBox4.Visible = True
+            Else
+                Label7.Visible = True
+                btnJugar.Visible = False
+
+                lblScore.Visible = False
+
+                PictureBox3.Visible = True
+                PictureBox1.Visible = False
+                PictureBox4.Visible = False
+
+                Beep()
+                System.Threading.Thread.Sleep(5000)
+                End
+
+            End If
+
+
+
+        End If
+
+
+
+
+
+
+    End Sub
+
+    Private Sub frmFortuna_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label5.Visible = False
+        Label7.Visible = False
+
+        Randomize()
+
+        lblScore.Text = score
+
+
+    End Sub
+
+End Class
